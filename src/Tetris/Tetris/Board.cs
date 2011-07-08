5ea57da6f7e2d3101e9cc29fa2afad03b3da9238
skiel85 <skiel85@a14b762e-eaac-d828-8968-3788fa2f5924>
2011-07-08 02:41:01 +0000

    Esbozo inicial.

---
+++ src/Tetris/Tetris/Board.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tetris
+{
+    public class Board
+    {
+        private Piece currentPiece;
+        public Piece CurrentPiece
+        {
+            get { return currentPiece; }
+            set { currentPiece = value; }
+        }
+
+        public void AddNewPiece()
+        {
+            currentPiece = new Piece();
+        }
+
+        public void Advance()
+        {
+            currentPiece.Height = 1;
+        }
+    }
+
+    public class Piece
+    {
+        int height;
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+    }
+}
---
+++ src/Tetris/TetrisTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tetris")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("Tetris")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM componenets.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b830e719-9385-4533-897e-9fb0990ae16b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
